515ff09d8c7e2f1d5c09db195845358275a0c011
thzzin <thithipaiva@hotmail.com>
2021-11-17 23:31:34 -0300

    tela home

---
+++ src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="divmain" >
+        <h1>Make Your Burguer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #divmain{
+        background-image: url('/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 90vh;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+    #divmain h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 60px;
+    background-color: #222;
+    padding: 20px 40px;
+    }
+</style>
---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Burger form component</p>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style  scoped>
+
+</style>
